ed8119b469b2bc4aa8b60135e90a02ac460c639c
Subkey <2822448396@qq.com>
2022-01-16 21:50:31 +0800

    fix: path problem

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 const http = require('http')
 const frequest = require('./frequest')
 const fs = require('fs')
+const path = require('path')
 
 const port = process.env.PORT || 2333
 const host = process.env.HOSTNAME || "localhost"
 
+
+
 const app = http.createServer((Req,Res)=>{
-  if(Req.url=='/favicon.ico') return
 
-  Res.setHeader('Access-Control-Allow-Origin','*')
-
-  let url = new URL(Req.url,`http://${Req.headers.host}`)
-  console.log(url.pathname+url.search)
-  if(url.pathname+url.search=='/') {
-    fs.readFile('./static/home.html',(err,data)=>{
+  /**
+   * 
+   * @param {string} relativePath for example `./static/favicon.ico`
+   * @param {string} fileMine for example `application/json`
+   */
+  const getStaticFile = (relativePath,fileMine) => {
+    fs.readFile(path.resolve(__dirname,relativePath),(err,data)=>{
       if(err) {
         Res.writeHead(404,{'content-type': 'text/plain'})
         Res.write(err.message)
       }
       else {
-        Res.writeHead(200,{'content-type': 'text/html'})
+        Res.writeHead(200,{'content-type': fileMine})
         Res.write(data)
       }
       Res.end()
     })
+  }
+
+  if(Req.url=='/favicon.ico') {
+    getStaticFile('./static/favicon.png','image/png')
+    return
+  }
+
+  let url = new URL(Req.url,`http://${Req.headers.host}`)
+  console.log(url.pathname+url.search)
+  if(url.pathname+url.search=='/') {
+    getStaticFile('./static/index.html','text/html')
     return
   }
 
